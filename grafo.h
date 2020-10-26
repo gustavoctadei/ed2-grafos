08a3bc0dcdf4657d9385479ea4e4665d34c5c967
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-10-26 18:05:47 -0400

    correcoes de identacao no header do grafo

--- grafo.h
+++ grafo.h
@@ -11,7 +11,9 @@ void busca_largura_grafo(Grafo *grafo, int inicio, int *visitado);
 
 //Função Principal para Menor Caminho entre dois Vértices utilizando o Algoritmo de Dijkstra
 void menor_caminho_grafo_dijkstra(Grafo *grafo, int inicial, int *antecessor, float *distancia);
+
 //Função Principal para Menor Caminho entre dois Vértices utilizando o Algoritmo de Floyd Warshall
 void menor_caminho_grafo_floyd_warshall(Grafo *grafo);
+
 //Arvore Geradora Minima - Algoritmo de Prim
 void arvore_geradora_minima_prim(Grafo *grafo, int origem, int *pai);
